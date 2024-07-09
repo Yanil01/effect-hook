ddebe5728e2b8556fe485ac280f16a541f31f20d
Yanil01 <idoyanil45@gmail.com>
2024-07-09 17:54:52 +0545

    done

--- src/Components/ClassCounteOne.js
+++ src/Components/ClassCounteOne.js
@@ -7,6 +7,7 @@ class ClassCounteOne extends Component {
     // Initialize state
     this.state = {
       count: 0,
+      name: "",
     };
   }
 
@@ -17,12 +18,23 @@ class ClassCounteOne extends Component {
 
   // Lifecycle method: runs after a component update
   componentDidUpdate(prevProps, prevState) {
-    document.title = `Clicked ${this.state.count} times`; // Update document title after state change
+    if (prevState.count !== this.state.count) {
+      console.log("Updating document title...");
+      document.title = `Clicked ${this.state.count} times`;
+    }
+    // Update document title after state change
   }
 
   render() {
     return (
       <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => {
+            this.setState({ name: e.target.value });
+          }}
+        />
         <button
           onClick={() => this.setState({ count: this.state.count + 1 })} // Increment count on button click
           style={{ cursor: "pointer" }}
--- src/Components/HookCounterOne.js
+++ src/Components/HookCounterOne.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from "react";
 
 function HookCounterOne() {
   const [count, setCount] = useState(0); // Define state variable 'count' initialized to 0
+  const [name, setName] = useState("");
   useEffect(() => {
+    console.log("useEffect - Updating document title");
     document.title = `You clicked ${count} times`; // Update the document title with the current count
   }); // useEffect runs after every render because it has no dependencies listed
 
   return (
     <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={() => setCount(count + 1)}>Count {count}</button>{" "}
       {/* Button to increment count */}
     </div>
